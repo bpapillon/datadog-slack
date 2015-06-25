829e78567a45d84b4045c3ecca71cd99c38365e3
Ben Papillon <bpapillon@umbel.com>
2015-06-24 20:45:03 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from datadog import statsd as dogstats
+from flask import Flask, request
+
+app = Flask(__name__)
+datadog.initialize(statsd_host='localhost', statsd_port=8126)
+
+
+@app.route('/message', methods=['GET', 'POST'])
+def message():
+    channel = request.form.get('channel_name')
+    if channel:
+        dogstats.increment('slack.channels.%s' % channel)
+
+if __name__ == '__main__':
+    app.run()
